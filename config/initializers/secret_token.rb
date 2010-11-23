51846e148ca59bb117ceb539016b04d548b0b56d
pushmatrix <daniel.beauchamp@gmail.com>
2010-11-22 23:30:44 -0500

    Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grantify::Application.config.secret_token = '037a26b8391e088d0571af680c17e07523517040a6a4656f147ad5c9036c9e108cabd8bfe180a7f6cf0cedc28a69e5b819f05aa1e658ddd80125682cfa1beceb'
