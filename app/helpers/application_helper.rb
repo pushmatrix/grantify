f765037ba73343bb9954bcb251ec9ed6cdda8bba
pushmatrix <daniel.beauchamp@gmail.com>
2010-11-29 18:30:03 -0500

    more styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def section_link(name,options)
+      if options[:action] == params[:action] and options[:controller] == params[:controller]
+        link_to "<div class='tab selected'>#{name}</div>".html_safe, options
+      else
+        link_to "<div class='tab'>#{name}</div>".html_safe, options
+      end
+  end
 end
